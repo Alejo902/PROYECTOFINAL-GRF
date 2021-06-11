481d90a958bb919847c14f56d793e649adbbc678
Alejo902 <alejomurcia902@hotmail.com>
2021-06-11 13:06:19 -0500

    CreeClases

--- sketch.js
+++ sketch.js
@@ -2,6 +2,12 @@ let bill, cinta, fuegoazul, fuegorosa, gnomo, portal, rosa, tienda, tiostan,
 winner, raton, pag_grande, muercie, mabel, hacha, gameO, dipper, diario_gran, alfombra,abuela,corazon,
 abuelo,cuarto,escogerP,hombreTauro,bosque,paginas,inicial,tronco;
 
+let enemigo = new Enemigos;
+let arma = new ArmasE;
+
+let vida = [];
+
+
 function preload(){
 bill=loadImage("/assets/0bill0.png");
 cinta=loadImage("/assets/0cinta0.png");
@@ -32,24 +38,70 @@ bosque=loadImage("/assets/Mapa Bosque-8.png");
 paginas=loadImage("/assets/PaginasDelLibro-8.png");
 inicial=loadImage("/assets/Pantalla inicial-8.png");
 tronco=loadImage("/assets/Tronco.png");
+maya=loadImage("/assets/maya-8.png");
 
 
 
 }
 function setup() {
   createCanvas(1000, 500);
+
+  for (let i = 0; i < 4; i++) {
+    let x = (i*76)+40;
+    let y = 40;
+    vida.push(new Vidas(x,y));
+  }
 }
 
-
 function draw() {
-  background(220);
 
-  ellipse(50,50,250,250);
-
-
-  image(tronco,20,20);
+  for (let i = 0; i < vida.length; i++) {
+     
+    vida[i].mostrar();
+  }
   
+switch (1) {
+  case 0:
+    
+    enemigo.mostrarRaton();
+
+  
+      break;
 
 
+  case 1:
+    enemigo.mostrarMurcie();
+      break;
 
+  case 2:
+      this.x+=5;
+      break;
+
+  case 3:
+      this.x+=5;
+      break;
+
+  case 4:
+      this.y-=5;
+      break;
+
+  case 5:
+      this.y-=5;
+      break;
+
+  case 6:
+      this.y+=5;
+      break;
+
+  case 7:
+      this.y+=5;
+      break;
+}
+}
+
+function validarTeclas() {
+  if (keyIsDown(97) || keyIsDown (65)) {
+     
+  
+  }
 }
